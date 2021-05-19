d714ccf2a1afb860d943c2945190b172af718ee2
Taani Maama <taani1maama@live.com>
2021-05-18 21:51:18 -0700

    Created the formatTime function.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [displayTime, setDisplayTime] = useState(1500);
+
+  const formatTime = time => {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    return (
+      (minutes < 10 ? "0" + minutes : minutes) +
+      ":" +
+      (seconds < 10 ? "0" + seconds : seconds)
+    );
+  };
+
+  return <div className="App">{formatTime(displayTime)}</div>;
+};
 
 export default App;
